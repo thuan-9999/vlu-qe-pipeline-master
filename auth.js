e8512e0989c1c071cf6ee35577ffe80080277468
thuan-9999 <nguyennguyen62thuan0@gmail.com>
2026-09-16 19:21:46 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ function login(username, password) {
   if (lockedAccounts.includes(username)) {
     return false; // Tài khoản bị khóa
   }
-  if (username === 'admin' && password === '9999') {
+  if (username === 'admin' && password === '123') {
     return true;
   }
   return false;
